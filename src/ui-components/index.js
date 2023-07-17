1713e34258c2a22597b4cec857e8c0839259ea32
Aryan Kumar <aryankumar877@gmail.com>
2023-07-17 15:44:43 +0530

    removed figma sign up and navlink components

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -3,32 +3,28 @@
  * Please refrain from making any modifications to this file.              *
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
-
-export { default as Filters } from "./Filters";
-export { default as NavLinkHover } from "./NavLinkHover";
-export { default as Logo } from "./Logo";
-export { default as HeroLayout2 } from "./HeroLayout2";
-export { default as SideBar } from "./SideBar";
-export { default as NavBarSide } from "./NavBarSide";
-export { default as LogoWithText } from "./LogoWithText";
+export { default as Authentication } from "./Authentication";
+export { default as ContactUs } from "./ContactUs";
+export { default as CTASection } from "./CTASection";
 export { default as DataRow } from "./DataRow";
+export { default as EditProfile } from "./EditProfile";
+export { default as Features2x2 } from "./Features2x2";
+export { default as Filters } from "./Filters";
+export { default as HeroLayout1 } from "./HeroLayout1";
+export { default as HeroLayout2 } from "./HeroLayout2";
+export { default as Logo } from "./Logo";
+export { default as LogoWithText } from "./LogoWithText";
+export { default as MarketingFooter } from "./MarketingFooter";
+export { default as MarketingFooterBrand } from "./MarketingFooterBrand";
+export { default as MarketingPricing } from "./MarketingPricing";
 export { default as MyIcon } from "./MyIcon";
 export { default as NavBarHeader } from "./NavBarHeader";
-export { default as HeroLayout1 } from "./HeroLayout1";
-export { default as ContactUs } from "./ContactUs";
-export { default as NavLink } from "./NavLink";
 export { default as NavBarHeader2 } from "./NavBarHeader2";
-export { default as SignIn } from "./SignIn";
-export { default as Signup } from "./Signup";
+export { default as NavBarSide } from "./NavBarSide";
 export { default as ProfileCard } from "./ProfileCard";
+export { default as SideBar } from "./SideBar";
 export { default as Stat } from "./Stat";
-export { default as CTASection } from "./CTASection";
-export { default as MarketingFooterBrand } from "./MarketingFooterBrand";
-export { default as MarketingFooter } from "./MarketingFooter";
-export { default as MarketingPricing } from "./MarketingPricing";
-export { default as Features2x2 } from "./Features2x2";
-export { default as EditProfile } from "./EditProfile";
 export { default as studioTheme } from "./studioTheme";
-export { default as SignUp } from "./SignUp";
-export { default as Authentication } from "./Authentication";
+export { default as UserCreateForm } from "./UserCreateForm";
 export { default as UserUpdateForm } from "./UserUpdateForm";
+
