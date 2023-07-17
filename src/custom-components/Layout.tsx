c449c284002b043dd5850d3bb3f05321d3b2f63e
swaraj bachu <rajswaraj.r@gmail.com>
2023-07-17 20:41:11 +0530

    style(navlinks): changed navlink colors to brand colors

--- src/custom-components/Layout.tsx
+++ src/custom-components/Layout.tsx
@@ -3,13 +3,14 @@ import { Flex, useTheme, View, withAuthenticator } from '@aws-amplify/ui-react';
 import React from 'react';
 // Layout.tsx
 import styles from '../styles/Layout.module.css';
+import NavItems from './NavItems';
 // import NavItems from './NavItems';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.layout}>
       <div className={styles.navbar}>
-        <NavBarSide />
+        <NavBarSide  children={<NavItems/>} />
       </div>
       <div className={styles.content}>{children}</div>
     </div>
---
+++ src/pages/audio-files.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AudioFiles() {
+  return (
+    <Layout>
+    <div>
+      Audio Files
+    </div>
+    </Layout>
+  )
+}
+
+export default AudioFiles
