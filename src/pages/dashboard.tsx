ba3db02b8a722b17de47df73e4dfd04a5c34d576
Aryan Kumar <aryankumar877@gmail.com>
2023-07-17 19:07:49 +0530

    name on dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,16 +2,18 @@ import Layout from '@/custom-components/Layout';
 // import NavItems from '@/mycomponents/NavItems';
 import NavBarSide from '@/ui-components/NavBarSide';
 import {
+  useAuthenticator,
   withAuthenticator,
   WithAuthenticatorProps,
 } from '@aws-amplify/ui-react';
 import React from 'react';
 
-function Dashboard({ user, signOut }: WithAuthenticatorProps) {
+function Dashboard() {
+  const { user } = useAuthenticator((context) => [context.user]);
   return (
     <Layout>
       <div>
-        <h1>Hello {user?.username}</h1>
+        <h1>Hello {user?.attributes?.name}</h1>
       </div>
     </Layout>
   );
