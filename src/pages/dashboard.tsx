2c3dbd8c6d4fd1a0c875b43159bab97facd54628
Aryan Kumar <aryankumar877@gmail.com>
2023-07-19 08:02:39 +0530

    primary color changed for text

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,6 +2,7 @@ import Layout from '@/custom-components/Layout';
 // import NavItems from '@/mycomponents/NavItems';
 import NavBarSide from '@/ui-components/NavBarSide';
 import {
+  Text,
   useAuthenticator,
   View,
   withAuthenticator,
@@ -13,7 +14,7 @@ function Dashboard() {
   const { user } = useAuthenticator((context) => [context.user]);
   return (
     <View>
-      <h1>Hello {user?.attributes?.name}</h1>
+      <Text>Hello {user?.attributes?.name}</Text>
     </View>
   );
 }
