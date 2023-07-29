368cad73bb2a6f766f600af24883d9a99be4605b
swaraj bachu <rajswaraj.r@gmail.com>
2023-07-29 09:14:44 +0530

    making it clean

--- src/custom-components/Layout.tsx
+++ src/custom-components/Layout.tsx
@@ -1,4 +1,4 @@
-import { NavBarHeader, NavBarSide } from '@/ui-components';
+import { NavBarHeader, NavBarSide } from "@/ui-components";
 import {
   Button,
   Card,
@@ -8,23 +8,24 @@ import {
   View,
   withAuthenticator,
   Text,
-} from '@aws-amplify/ui-react';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { BiLeftArrow, BiLeftArrowAlt } from 'react-icons/bi';
-import { BsBack } from 'react-icons/bs';
-import { IoIosArrowBack } from 'react-icons/io';
-import styles from '../styles/Layout.module.css';
-import NavItems from './NavItems';
-import { AnimatePresence, motion } from 'framer-motion';
-import  { HiMiniBars2 } from 'react-icons/hi2';
-import Image from 'next/image';
+} from "@aws-amplify/ui-react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { BiLeftArrow, BiLeftArrowAlt } from "react-icons/bi";
+import { BsBack } from "react-icons/bs";
+import { IoIosArrowBack } from "react-icons/io";
+import styles from "../styles/Layout.module.css";
+import NavItems from "./NavItems";
+import { AnimatePresence, motion } from "framer-motion";
+import { HiMiniBars2 } from "react-icons/hi2";
+import { MdOutlineClose } from "react-icons/md";
+import Image from "next/image";
 
 function Layout({ children }: { children: React.ReactNode }) {
   const { tokens } = useTheme();
   const router = useRouter();
   const [isNavOpen, setIsNavOpen] = React.useState(false);
-  
+
   useEffect(() => {
     window.addEventListener(
       "resize",
@@ -32,52 +33,63 @@ function Layout({ children }: { children: React.ReactNode }) {
     );
   }, []);
 
+  //on page change navlinks will be set to false, but we can check a better way to do this
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setIsNavOpen(false);
+    };
+    router.events.on("routeChangeStart", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
-     <View boxShadow="medium" className={styles.header}>
-      {/* <Image src="/logo.png" alt="brand" className={styles.logo} width={10} height={8} /> */}
-      <Text className={styles.title}>
-        Decibel
-      </Text>
-      <button
-        onClick={() => setIsNavOpen(!isNavOpen)}
-        className={styles.iconButton}
-      >
-        <HiMiniBars2 className={styles.icon} />
-      </button>
-    </View>
-    <AnimatePresence>
-  {isNavOpen && (
-    <motion.div
-    className={styles.mobileNav}
-    initial={{ height: 0 }}
-    animate={{ height: "auto" }}
-    exit={{ height: 0 }}
-    transition={{ duration: 0.3, ease: "easeInOut" }}
-    >
-      <NavItems />
-    </motion.div>
-  )}
-</AnimatePresence>
-
-
-
-
+      <View boxShadow="medium" className={styles.header}>
+        {/* <Image src="/logo.png" alt="brand" className={styles.logo} width={10} height={8} /> */}
+        <Text className={styles.title}>Decibel</Text>
+        <Button
+          border="none"
+          borderRadius="10px"
+          onClick={() => setIsNavOpen(!isNavOpen)}
+          className={styles.iconButton}
+        >
+          {isNavOpen ? (
+            <MdOutlineClose className={styles.icon} />
+          ) : (
+            <HiMiniBars2 className={styles.icon} />
+          )}
+        </Button>
+      </View>
+      <AnimatePresence>
+        {isNavOpen && (
+          <motion.div
+            className={styles.mobileNav}
+            initial={{ height: 0 }}
+            animate={{ height: "auto" }}
+            exit={{ height: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+          >
+            <NavItems />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       <NavBarSide
         backgroundColor={tokens.colors.brand.secondary[10]}
         overrides={{
           NavBarSide: {
-            height: '100vh',
-            borderRadius: '0',
+            height: "100vh",
+            borderRadius: "0",
           },
           Button: {
-            borderRadius: '24px',
-            fontSize: '16px',
+            borderRadius: "24px",
+            fontSize: "16px",
           },
-          'Wesley Peck': {
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
+          "Wesley Peck": {
+            textTransform: "uppercase",
+            fontWeight: "bold",
           },
         }}
         columnStart="1"
@@ -85,7 +97,7 @@ function Layout({ children }: { children: React.ReactNode }) {
         position="fixed"
         left="0"
         overflow="auto"
-        width={{ base: '0px', medium: '300px' }}
+        width={{ base: "0px", medium: "300px" }}
         maxWidth="320px"
         className={styles.sidebar}
       >
@@ -106,7 +118,7 @@ function Layout({ children }: { children: React.ReactNode }) {
         top="80px"
         bottom="0"
         right="0"
-        left={{ base: '0', medium: '300px' }}
+        left={{ base: "0", medium: "300px" }}
         position="fixed"
         className={styles.content}
         style={{
@@ -131,7 +143,7 @@ function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default withAuthenticator(Layout, {
-  loginMechanisms: ['email'],
-  signUpAttributes: ['name', 'email'],
-  socialProviders: ['google'],
+  loginMechanisms: ["email"],
+  signUpAttributes: ["name", "email"],
+  socialProviders: ["google"],
 });
